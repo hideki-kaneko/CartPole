2c85ac0f04e624143445e6f42e1b5cddf316f671
hideki-kaneko <hideki-kaneko@users.noreply.github.com>
2020-01-12 13:38:52 +0900

    左右キーで動くカートを追加

---
+++ Assets/Scripts/CartBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartBehavior : MonoBehaviour
+{
+    public float Speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Speed;
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        Vector3 force = new Vector3(x, 0, 0);
+        rigidbody.AddForce(force);
+    }
+}
