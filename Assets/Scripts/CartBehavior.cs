d3c60e4cae30927f3821f5581bfe3b1d3b368c2d
hideki-kaneko <hideki-kaneko@users.noreply.github.com>
2020-01-12 17:28:39 +0900

    初期位置をランダムにする関数を追加

--- Assets/Scripts/CartBehavior.cs
+++ Assets/Scripts/CartBehavior.cs
@@ -7,14 +7,35 @@ public class CartBehavior : MonoBehaviour
     // Public
     public float Speed = 1;
     public GameObject mObjPole;
+    public float randomInitPosXMax = 1;
+    public float initPosY = 0;
 
     // Private
     private Vector3 initDir;
 
+    // ランダムな初期位置をセットする
+    public void SetRandomCartPosition()
+    {
+        float initX = Random.Range(-randomInitPosXMax, randomInitPosXMax);
+        Vector3 initPos = new Vector3(initX, initPosY, 0);
+        Rigidbody rigidbodyCart = GetComponent<Rigidbody>();
+        rigidbodyCart.transform.position = initPos;
+    }
+
+    // 棒の初期位置からの傾き（度）を取得する
+    private float calcPoleAngle_()
+    {
+        Rigidbody rigidbodyPole = mObjPole.GetComponent<Rigidbody>();
+        Vector3 currentDir = rigidbodyPole.transform.up;
+        float rotDeg = Mathf.Abs(Vector3.Angle(currentDir, initDir));
+
+        return rotDeg;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        SetRandomCartPosition();
         Rigidbody rigidbodyPole = mObjPole.GetComponent<Rigidbody>();
         initDir = rigidbodyPole.transform.up;
     }
@@ -31,9 +52,7 @@ public class CartBehavior : MonoBehaviour
 
     private void OnGUI()
     {
-        Rigidbody rigidbodyPole = mObjPole.GetComponent<Rigidbody>();
-        Vector3 currentDir = rigidbodyPole.transform.up;
-        float rotDeg = Mathf.Abs(Vector3.Angle(currentDir, initDir));
+        float rotDeg = calcPoleAngle_();
         string textRot = string.Format("Rotation: {0:F2}", rotDeg);
 
         GUI.Box(new Rect(10, 10, 120, 90), "Menu");
