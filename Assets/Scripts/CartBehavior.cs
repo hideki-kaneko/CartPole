4aa3e651c3bedec374edc619852e858043bcadd8
hideki-kaneko <hideki-kaneko@users.noreply.github.com>
2020-01-12 16:36:18 +0900

    ポールの回転角をデバッグ表示するようにした

--- Assets/Scripts/CartBehavior.cs
+++ Assets/Scripts/CartBehavior.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class CartBehavior : MonoBehaviour
 {
+    // Public
     public float Speed = 1;
+    public GameObject mObjPole;
+
+    // Private
+    private Vector3 initDir;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody rigidbodyPole = mObjPole.GetComponent<Rigidbody>();
+        initDir = rigidbodyPole.transform.up;
     }
 
     // Update is called once per frame
@@ -20,5 +27,16 @@ public class CartBehavior : MonoBehaviour
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         Vector3 force = new Vector3(x, 0, 0);
         rigidbody.AddForce(force);
+    }
+
+    private void OnGUI()
+    {
+        Rigidbody rigidbodyPole = mObjPole.GetComponent<Rigidbody>();
+        Vector3 currentDir = rigidbodyPole.transform.up;
+        float rotDeg = Mathf.Abs(Vector3.Angle(currentDir, initDir));
+        string textRot = string.Format("Rotation: {0:F2}", rotDeg);
+
+        GUI.Box(new Rect(10, 10, 120, 90), "Menu");
+        GUI.Label(new Rect(20, 30, 100, 90), textRot);
     }
 }
