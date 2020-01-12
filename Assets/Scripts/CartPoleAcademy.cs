d7f68d5b318e7d3e0eb97b22e667fba49d1f673e
hideki-kaneko <hideki-kaneko@users.noreply.github.com>
2020-01-12 17:09:22 +0900

    ML-Agentsアセットを追加

---
+++ Assets/Scripts/CartPoleAcademy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class CartPoleAcademy : Academy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+}
